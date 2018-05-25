a0a46b261adb476c4204d770da6f589b2a8979bb
mldn <mldnqa@163.com>
2018-05-26 00:28:47 +0800

    Me Change haha

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello World !") ;
+		System.out.println("www.mldn.cn") ;
+		System.out.println("小王同学晚上给大家做服务。你们懂的。") ;
 	}
 }
